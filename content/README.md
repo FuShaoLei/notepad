5ee5d2b83af0c219d8d24996f20dbef930599671
fushaolei <1563250958@qq.com>
2024-02-07 01:20:30 +0800

    update

---
+++ content/README.md
@@ -0,0 +1,3 @@
+## NotePad
+
+本网页由自己做的[ole](https://github.com/FuShaoLei/ole)打造
---
+++ deploy.sh
@@ -0,0 +1,13 @@
+ole -g='F:\notepad'
+cd publish
+git init
+git remote add origin git@github.com:FuShaoLei/notepad.git
+git add .
+git commit -m 'update blog'
+git push -f --set-upstream origin master:page
+cd ..
+rm -rf publish
+git add .
+git commit -m "update"
+git push
+
